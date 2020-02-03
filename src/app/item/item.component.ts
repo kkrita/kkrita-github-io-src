b02cb38edb4b8a36e44dbc835ab32248b11f1daf
kushal <kushaldeveloper@gmail.com>
2020-02-03 08:37:36 -0700

    add changes

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getUrlData();
+  }
+
+  getUrlData() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.name = params.get('id');
+    });
   }
 
 }
