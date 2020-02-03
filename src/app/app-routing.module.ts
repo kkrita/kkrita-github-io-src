561f50bcd29e72ad4c70b467add0261548c20ce1
kushal <kushaldeveloper@gmail.com>
2020-02-03 08:29:44 -0700

    add changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemComponent } from './item/item.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "item/:id", component: ItemComponent },
+  { path: "", component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
